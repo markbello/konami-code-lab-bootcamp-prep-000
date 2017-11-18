8fe9d4fb9b217c3f2a5d9f17497684a4e35e049c
Mark Bello <mcbello51286@gmail.com>
2017-11-18 23:05:02 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   let index = 0;
   // Write your JavaScript code inside the init() function
-  document.body.addEventListener('keydown', function(e){
+  document.addEventListener('keydown', function(e){
     const key = parseInt(e.detail || e.which);
       if (key === code[index]) {
         index++;
@@ -15,5 +15,5 @@ function init() {
         index = 0;
       }
     }
-  })
+  });
 }
